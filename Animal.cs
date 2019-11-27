9adbc5c8f894a7916ee7f02f34484d8ac9f58339
David Andersson <david.andersson@zocom.se>
2019-11-27 14:12:02 +0100

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercisesOop
+{
+	class Animal
+	{
+		// In larger projects, avoid using public - it's a security risk
+		public string name;
+		public string species;
+		public bool hasLegs;
+
+		public Animal(string name, string  species, bool hasLegs)
+		{
+			this.name = name;
+			this.species = species;
+			this.hasLegs = hasLegs;
+		}
+
+		public void PrintProps()
+		{
+			string legStatus = "does not";
+			if (hasLegs) { legStatus = "does"; }
+			Console.WriteLine($"{name} is a {species} and it {legStatus} have legs.");
+		}
+		// see also ToString()
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExercisesOop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Exercises in OOP");
+			Animal cat = new Animal("David", "cat", true);
+			cat.PrintProps();
+			Animal fish = new Animal("Nemo", "clownfish", false);
+			fish.PrintProps();
+		}
+	}
+}
